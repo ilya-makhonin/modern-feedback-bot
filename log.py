5a8bc18e5b83302898a5ba7dda00828578bbf176
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-15 22:18:40 +0300

    Created function for working with logging module

--- log.py
+++ log.py
@@ -2,5 +2,24 @@ import logging
 import os
 
 
-def log():
-    pass
+def log(name='main', file='logs.log', level='DEBUG'):
+    logging_types = dict(
+        DEBUG=logging.DEBUG,
+        INFO=logging.INFO,
+        WARNING=logging.WARNING,
+        ERROR=logging.ERROR,
+        CRITICAL=logging.CRITICAL)
+
+    if not os.path.exists('logs/'):
+        os.mkdir('./logs/')
+
+    logger = logging.getLogger(name)
+    logging_level = logging_types.get(level) if logging_types.get(level) else logging_types.get('DEBUG')
+    logger.setLevel(logging_level)
+    file_handler = logging.FileHandler(f"./logs/{file}")
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
