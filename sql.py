fa32e3c63188aafa2d29ba7c712b7a49f6f74d78
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-16 23:46:18 +0300

    Beginning of creation some functions for working with DB and adding user's info

--- sql.py
+++ sql.py
@@ -1,2 +1,13 @@
 import pymysql
 from log import log
+
+
+sql_log = log('sql', 'sql.log', 'DEBUG')
+
+
+def connection():
+    return pymysql.connect('connect string')
+
+
+def add_user():
+    pass
