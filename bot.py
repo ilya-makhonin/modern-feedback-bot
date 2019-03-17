8c96929b8e8087f4bfc090af77f6d07e52893407
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-18 02:36:20 +0300

    Added admin's handlers for mailing and getting user's count. Updated logging - getting more info

--- bot.py
+++ bot.py
@@ -12,25 +12,58 @@ logger = log('bot', 'bot.log', 'INFO')
 
 @bot.message_handler(commands=['start'])
 def start_handler(message: telebot.types.Message):
-    bot.send_message(message.from_user.id, start_mess)
-    logger.info(f"It's start handler. Message from {message.from_user.id}")
+    adding = sql.add_user(
+        message.from_user.id, message.from_user.first_name, message.from_user.last_name, message.from_user.username)
+    if adding:
+        bot.send_message(message.from_user.id, start_mess)
+        logger.info(f"It's start handler. User {message.from_user.id} had added to the bot")
+        return
+    sql.sql_log.error(f"Error in start handler! User {message.from_user.id} did not have added to the bot")
 
 
 @bot.message_handler(commands=['help'])
 def help_handler(message: telebot.types.Message):
     bot.send_message(message.from_user.id, help_mess)
-    logger.info(f"It's help handler. Message from {message.from_user.id}")
+    logger.info(f"It's help handler. Message from user {message.from_user.id}")
+
+
+@bot.message_handler(commands=['user_count'])
+def get_user_count(message: telebot.types.Message):
+    logger.info(f"User {message.from_user.id} had entered /user_count command")
+    if message.from_user.id in sql.get_admins():
+        user_count = sql.user_count()
+        bot.send_message(message.from_user.id, count_mess.format(user_count))
+        logger.info(f"User {message.from_user.id} had gotten user's count. Result: {user_count}")
+
+
+@bot.message_handler(commands=['global'])
+def global_mailing(message: telebot.types.Message):
+    logger.info(f"User {message.from_user.id} had entered /global command")
+    if message.from_user.id in sql.get_admins():
+        text = (message.text[7:]).strip()
+        deleted_user = 0
+        users = sql.get_users()
+        for user in users:
+            try:
+                bot.send_message(user, text)
+                logger.info(f"User {user} is using the bot. A message had sent successfully")
+            except Exception as error:
+                deleted_user += 1
+                logger.info(f"User {user} is'nt using the bot. "
+                            f"Error in global mailing function: {error.with_traceback(None)}")
+        bot.send_message(message.from_user.id, deleted_mess.format(deleted_user))
+        logger.info(f"User {message.from_user.id} had sent global mailing. Text: {text}. Deleted user: {deleted_user}")
 
 
 @bot.message_handler(content_types=['sticker'])
 def sticker_handler(message: telebot.types.Message):
-    logger.info(f"It's sticker handler. Message from {message.from_user.id}")
     try:
         if message.chat.id == int(CHAT):
             bot.send_sticker(message.reply_to_message.forward_from.id, message.sticker.file_id)
-            logger.info(f"In CHAT. Info: {message}")
+            logger.info(f"Sticker handler. In CHAT. Info: {message}")
         else:
             bot.forward_message(CHAT, message.chat.id, message.message_id)
+            bot.reply_to(message, success_mess)
             logger.info(f"Sticker handler. Message from a user. Info: {message}")
     except Exception as error:
         logger.info(f"Exception in sticker handler. Info: {error.with_traceback(None)}")
@@ -38,13 +71,13 @@ def sticker_handler(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['photo'])
 def images_handler(message: telebot.types.Message):
-    logger.info(f"It's images handler. Message from {message.from_user.id}")
     try:
         if message.chat.id == int(CHAT):
             bot.send_photo(message.reply_to_message.forward_from.id, message.photo[-1].file_id)
-            logger.info(f"In CHAT. Info: {message}")
+            logger.info(f"Photo handler. In CHAT. Info: {message}")
         else:
             bot.forward_message(CHAT, message.chat.id, message.message_id)
+            bot.reply_to(message, success_mess)
             logger.info(f"Image handler. Message from a user. Info: {message}")
     except Exception as error:
         logger.info(f"Exception in image handler. Info: {error.with_traceback(None)}")
@@ -52,13 +85,13 @@ def images_handler(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['document'])
 def file_handler(message: telebot.types.Message):
-    logger.info(f"It's file handler. Message from {message.from_user.id}")
     try:
         if message.chat.id == int(CHAT):
             bot.send_document(message.reply_to_message.forward_from.id, message.document.file_id)
-            logger.info(f"In CHAT. Info: {message}")
+            logger.info(f"Document handler. In CHAT. Info: {message}")
         else:
             bot.forward_message(CHAT, message.chat.id, message.message_id)
+            bot.reply_to(message, success_mess)
             logger.info(f"File handler. Message from a user. Info: {message}")
     except Exception as error:
         logger.info(f"Exception in file handler. Info: {error.with_traceback(None)}")
@@ -66,13 +99,15 @@ def file_handler(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['audio'])
 def audio_handler(message: telebot.types.Message):
-    logger.info(f"It's audio handler. Message from {message.from_user.id}")
     try:
         if message.chat.id == int(CHAT):
             bot.send_audio(message.reply_to_message.forward_from.id, message.audio.file_id)
-            logger.info(f"In CHAT. Info: {message}")
+            logger.info(f"Audio handler. In CHAT. Info: {message}")
         else:
-            bot.forward_message(CHAT, message.chat.id, message.message_id)
+            bot.send_audio(CHAT, message.audio.file_id,
+                           caption=info_mess.format(message.from_user.first_name, message.from_user.username),
+                           reply_to_message_id=message.from_user.id)
+            bot.send_message(message.from_user.id, other_mess)
             logger.info(f"Audio handler. Message from a user. Info: {message}")
     except Exception as error:
         logger.info(f"Exception in audio handler. Info: {error.with_traceback(None)}")
@@ -80,33 +115,39 @@ def audio_handler(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['voice'])
 def voice_handler(message: telebot.types.Message):
-    logger.info(f"It's voice handler. Message from {message.from_user.id}")
     try:
         if message.chat.id == int(CHAT):
             bot.send_voice(message.reply_to_message.forward_from.id, message.voice.file_id)
-            logger.info(f"In CHAT. Info: {message}")
+            logger.info(f"Voice handler. In CHAT. Info: {message}")
         else:
             bot.forward_message(CHAT, message.chat.id, message.message_id)
+            bot.reply_to(message, success_mess)
             logger.info(f"Voice handler. Message from a user. Info: {message}")
     except Exception as error:
         logger.info(f"Exception in voice handler. Info: {error.with_traceback(None)}")
 
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(content_types=['text'])
 def text_handler(message: telebot.types.Message):
-    logger.info(f"It's text handler. Message from {message.from_user.id}")
     try:
         if message.chat.id == int(CHAT):
             bot.send_message(message.reply_to_message.forward_from.id, message.text)
-            logger.info(f"In CHAT. Info: {message}")
+            logger.info(f"Text handler. In CHAT. Info: {message}")
         else:
             bot.forward_message(CHAT, message.chat.id, message.message_id)
+            bot.reply_to(message, success_mess)
             logger.info(f"Text handler. Message from a user. Info: {message}")
     except Exception as error:
         logger.info(f"Exception in text handler. Info: {error.with_traceback(None)}")
 
 
-def create_bot_instance(logging_enable=True):
+@bot.message_handler(func=lambda message: True)
+def other_handler(message: telebot.types.Message):
+    bot.send_message(message.from_user.id, other_mess)
+    logger.info(f"Other handler. Message from a user. Info: {message}")
+
+
+def create_bot_instance(logging_enable=True, logging_level='DEBUG'):
     if logging_enable:
-        telebot.logger.setLevel(logging.getLevelName('DEBUG'))
+        telebot.logger.setLevel(logging.getLevelName(logging_level))
     return bot
