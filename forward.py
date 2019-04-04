ed58287cbaef651e351857327f8d0a6d013289f5
Ilia Makhonin <noizemkutane77@gmail.com>
2019-04-04 15:02:16 +0300

    Updated Exception's processing, develop_debugging method and delete_data method

--- forward.py
+++ forward.py
@@ -3,9 +3,6 @@ from log import log
 
 
 class Forward:
-    """
-    This class needs to improve the communication method user_id <=> forward_message
-    """
     def __init__(self, develop_mode: bool):
         self.message_forward_data = dict()
         self.__develop_mode = develop_mode
@@ -13,8 +10,8 @@ class Forward:
 
     def develop_debugging(self, method: str, info: str):
         print('Method: ', method)
-        print('Global state now: ', self.message_forward_data)
         print('Info about this process: ', info)
+        print('Global state now: ', self.message_forward_data)
 
     def add_key(self, message: types.Message):
         new_key = {
@@ -29,26 +26,26 @@ class Forward:
             self.develop_debugging('add_key', f"Adding new key {message.date} => {str(new_key)}")
         self.logger.info(f"Method add_key. Information: {message.date} => {message.from_user.id}")
 
-    def get_id(self, message):
+    def get_id(self, message: types.Message):
         try:
             result: dict = self.message_forward_data.get(message.reply_to_message.forward_date)
             if self.__develop_mode:
                 self.develop_debugging('get_id', f"Getting id from {str(result)}")
             self.logger.info(f"Method get_id. Information: {result}")
             return result.get('id')
-        except KeyError as error:
+        except Exception as error:
             self.logger.info(f"Error from get_key method. Information {error.with_traceback(None)}")
 
-    def delete_data(self, message):
+    def delete_data(self, message: types.Message):
         try:
-            result = self.message_forward_data.pop(message.reply_to_message.date)
+            result = self.message_forward_data.pop(message.reply_to_message.forward_date)
             if self.__develop_mode:
                 self.develop_debugging(
                     'deleted_data',
                     f"Deleting data about a user by date: {str(message.reply_to_message.date)} => {str(result)}")
             self.logger.info(f"Method delete_data. "
                              f"Information: user id {result} was deleted by {message.reply_to_message.date}")
-        except KeyError as error:
+        except Exception as error:
             self.logger.info(f"Error from delete_data method. Information {error.with_traceback(None)}")
 
     def clear_data(self):
